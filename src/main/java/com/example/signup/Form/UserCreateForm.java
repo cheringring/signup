e9a63de1e57ee9a0e81d75d83e7a80e5fd8c8bb3
cheringring <gksmfthsu000@gmail.com>
2025-02-22 18:44:08 +0900

    nickname_^$%#

--- src/main/java/com/example/signup/Form/UserCreateForm.java
+++ src/main/java/com/example/signup/Form/UserCreateForm.java
@@ -11,7 +11,7 @@ public class UserCreateForm {
 
     @NotEmpty(message = "아이디는 필수입니다.")
     @Size(min = 3, max = 20, message = "아이디는 3-20자 사이여야 합니다.")
-    @Pattern(regexp = "^[a-zA-Z0-9가-힣]+$", message = "아이디는 한글, 영문, 숫자만 사용할 수 있습니다.")
+    @Pattern(regexp = "^[A-Za-z0-9\\u3131-\\u318E\\uAC00-\\uD7A3]+$", message = "아이디는 한글, 영문, 숫자만 사용할 수 있습니다.")
     private String userId;
 
     @NotEmpty(message = "이름은 필수 항목입니다.")
@@ -19,7 +19,7 @@ public class UserCreateForm {
 
     @NotEmpty(message = "닉네임은 필수 항목입니다.")
     @Size(min = 2, max = 20, message = "닉네임은 2-20자 사이여야 합니다.")
-    @Pattern(regexp = "^[a-zA-Z0-9가-힣]+$", message = "닉네임은 한글, 영문, 숫자만 사용할 수 있습니다.")
+    @Pattern(regexp = "^[A-Za-z0-9\\u3131-\\u318E\\uAC00-\\uD7A3]+$", message = "닉네임은 한글, 영문, 숫자만 사용할 수 있습니다.")
     private String nickname;
 
     @NotEmpty(message = "비밀번호는 필수 항목입니다.")
